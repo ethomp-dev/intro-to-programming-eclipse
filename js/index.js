571ec7a32f0dccdb5eea395718497acefd71e18b
E Thompson <ethompson.dev@gmail.com>
2021-06-02 20:53:16 -0400

    feat: add message form with event handling

--- js/index.js
+++ js/index.js
@@ -39,8 +39,70 @@
     }
   }
 
+  // CHALLENGE 3. Register Message Form
+  // --------------------------------------------------------------
+  const BUTTON = {
+    edit: 'edit',
+    save: 'save',
+    remove: 'remove',
+  }
+
+  const renderMessageForm = () => {
+    const messageForm = document.querySelector('form[name=leave_message]')
+
+    messageForm.addEventListener('submit', (event) => {
+      event.preventDefault()
+
+      const { name, email, message } = event.target
+
+      // check for missing fields
+      if (!name || !email || !message) {
+        return
+      }
+
+      const messageSection = document.querySelector('#messages')
+      const messageList = messageSection.querySelector('ul')
+
+      // if hidden, show the "messages" section
+      if (messageSection.style.display === 'none') {
+        messageSection.style.display = 'block'
+      }
+
+      // create new message
+      const newMessage = document.createElement('li')
+      newMessage.innerHTML = `<a href="mailto:${email.value}">${name.value}</a> wrote: <span>${message.value}</span> &nbsp;`
+
+      // create remove button
+      const removeButton = document.createElement('button')
+      removeButton.innerText = BUTTON.remove
+      removeButton.type = 'button'
+
+      removeButton.addEventListener('click', (event) => {
+        const entry = event.target.parentNode
+        const list = message.parentNode
+
+        // if there are no other messages, hide the section
+        if (list.childNodes.length <= 1) {
+          messageSection.style.display = 'none'
+        }
+
+        // remove entry from the list
+        entry.remove()
+      })
+
+      newMessage.appendChild(removeButton)
+
+      // add new message to the list
+      messageList.appendChild(newMessage)
+
+      // reset form
+      event.target.reset()
+    })
+  }
+
   document.addEventListener('DOMContentLoaded', () => {
     renderCopyright()
     renderSkillsList()
+    renderMessageForm()
   })
 })()
