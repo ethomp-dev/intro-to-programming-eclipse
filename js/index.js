af6b376ea55b14c1fb4599a61837f4d230791d1a
E Thompson <ethompson.dev@gmail.com>
2021-06-02 20:50:55 -0400

    feat: move logic into functions on DOM loaded

--- js/index.js
+++ js/index.js
@@ -1,40 +1,46 @@
 (() => {
   // CHALLENGE 1. Insert Copyright Text in Footer
   // --------------------------------------------------------------
+  const renderCopyright = () => {
+    const today = new Date()
+    const thisYear = today.getFullYear()
 
-  const today = new Date()
-  const thisYear = today.getFullYear()
+    // DOM selection
+    const footer = document.querySelector('footer')
 
-  // DOM selection
-  const footer = document.querySelector('footer')
+    // DOM manipulation (create)
+    const paragraph = document.createElement('p')
+    paragraph.innerHTML = `&copy; Elliot Thompson ${thisYear}`
 
-  // DOM manipulation (create)
-  const paragraph = document.createElement('p')
-  paragraph.innerHTML = `&copy; Elliot Thompson ${thisYear}`
-
-  // DOM manipulation (insert)
-  footer.appendChild(paragraph)
-
+    // DOM manipulation (insert)
+    footer.appendChild(paragraph)
+  }
 
   // CHALLENGE 2. Create List of Skills
   // --------------------------------------------------------------
+  const renderSkillsList = () => {
+    const skills = [
+      'HTML',
+      'JavaScript',
+      'CSS',
+    ]
 
-  const skills = [
-    'HTML',
-    'JavaScript',
-    'CSS',
-  ]
+    // DOM selection
+    const skillsSection = document.querySelector('#skills')
+    const skillsList = skillsSection.querySelector('ul')
 
-  // DOM selection
-  const skillsSection = document.querySelector('#skills')
-  const skillsList = skillsSection.querySelector('ul')
+    for (let i = 0; i < skills.length; i++) {
+      // DOM manipulation (create)
+      const skill = document.createElement('li')
+      skill.innerText = skills[i]
 
-  for (let i = 0; i < skills.length; i++) {
-    // DOM manipulation (create)
-    const skill = document.createElement('li')
-    skill.innerText = skills[i]
-
-    // DOM manipulation (modify)
-    skillsList.appendChild(skill)
+      // DOM manipulation (modify)
+      skillsList.appendChild(skill)
+    }
   }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    renderCopyright()
+    renderSkillsList()
+  })
 })()
